54f902b99f2e6b8a19b9e845d1bf49e93f72053b
JIHOI-KIM <pascal4847@gmail.com>
2021-09-20 04:55:22 +0900

    runner with timeout

--- analyzer.sh
+++ analyzer.sh
@@ -14,36 +14,36 @@ for file in $1/*.lua; do
 	let COUNT++
 	echo -n "[$COUNT] Examing file" $file" ..."
 
-	clang64lua $file 2>temp1.log 1>/dev/null
-	clang64lua $file 2>temp2.log 1>/dev/null
-	clang64lua $file 2>temp3.log 1>/dev/null
+	timeout 5s clang64lua $file 2>temp1.log 1>/dev/null
+	timeout 5s clang64lua $file 2>temp2.log 1>/dev/null
+	timeout 5s clang64lua $file 2>temp3.log 1>/dev/null
 	python3 asan.py $file clang64lua
 	
 	rm temp1.log
 	rm temp2.log
 	rm temp3.log
 
-	clang32lua $file 2>temp1.log 1>/dev/null
-	clang32lua $file 2>temp2.log 1>/dev/null
-	clang32lua $file 2>temp3.log 1>/dev/null
+	timeout 5s clang32lua $file 2>temp1.log 1>/dev/null
+	timeout 5s clang32lua $file 2>temp2.log 1>/dev/null
+	timeout 5s clang32lua $file 2>temp3.log 1>/dev/null
 	python3 asan.py $file clang32lua
 	
 	rm temp1.log
 	rm temp2.log
 	rm temp3.log
 
-	gcc64lua $file 2>temp1.log 1>/dev/null
-	gcc64lua $file 2>temp2.log 1>/dev/null
-	gcc64lua $file 2>temp3.log 1>/dev/null
+	timeout 5s gcc64lua $file 2>temp1.log 1>/dev/null
+	timeout 5s gcc64lua $file 2>temp2.log 1>/dev/null
+	timeout 5s gcc64lua $file 2>temp3.log 1>/dev/null
 	python3 asan.py $file gcc64lua
 	
 	rm temp1.log
 	rm temp2.log
 	rm temp3.log
 
-	gcc32lua $file 2>temp1.log 1>/dev/null
-	gcc32lua $file 2>temp2.log 1>/dev/null
-	gcc32lua $file 2>temp3.log 1>/dev/null
+	timeout 5s gcc32lua $file 2>temp1.log 1>/dev/null
+	timeout 5s gcc32lua $file 2>temp2.log 1>/dev/null
+	timeout 5s gcc32lua $file 2>temp3.log 1>/dev/null
 	python3 asan.py $file gcc32lua
 	
 	rm temp1.log
@@ -55,4 +55,5 @@ for file in $1/*.lua; do
 
 done
 
-echo "Created File: out.csv"
+mv out.csv out_$1.csv
+echo "Created File: out_$1.csv"
